18cea8ddca602f683a17249034e9eceb3f68c615
Dyovana <dyo.menegatti@hotmail.com>
2026-02-18 20:53:20 -0300

    feat: cria componente de botao

---
+++ src/components/AppButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-btn
+    :append-icon="appendIcon"
+    :variant="variant"
+    :disabled="disabled"
+    :loading="loading"
+    :intent="intent"
+    :rounded="rounded"
+    :color="colorBtn"
+    @click="$emit('click')"
+  >
+    {{ title }}
+  </v-btn>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  appendIcon: {
+    type: String,
+    default: 'undefined'
+  },
+  variant: {
+    type: String,
+    default: 'tonal'
+  },
+  intent: {
+    type: String,
+    default: 'primary'
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  },
+  title: String,
+  rounded: String,
+});
+
+defineEmits(['click']);
+
+const colorBtn = computed(() => {
+  if(props.intent === 'primary') return 'indigo-darken-3';
+});
+</script>
+
+<style>
+
+</style>
